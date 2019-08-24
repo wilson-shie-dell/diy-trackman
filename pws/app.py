fa09f43f0b7a2762c3c389fcce695bbffe039553
Wilson Shie <wilson.shie@dell.com>
2019-08-24 20:09:40 +0900

    Initail integration with MongoDB/mlab

--- pws/app.py
+++ pws/app.py
@@ -1,24 +1,72 @@
 import os
 import uuid
+import time
+import json
 from flask import Flask
+from pymongo import MongoClient
+import pymongo
 
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
 
+# If running in PWS, use mlab
+if 'VCAP_SERVICES' in os.environ:
+    VCAP_SERVICES = json.loads(os.environ['VCAP_SERVICES'])
+    MONCRED = VCAP_SERVICES["mlab"][0]["credentials"]
+    client = MongoClient(MONCRED["uri"])
+    DB_NAME = str(MONCRED["uri"].split("/")[-1])
+    db = client[DB_NAME]
+
+else:
+    client = MongoClient('192.168.10.9:27017')
+    db = client.rpi_trackman
+    #print(db)
+
+
 @app.route('/')
 def mainmenu():
 
-    response = """
+    uid = int(time.time())
+
+    # Entry needed in DB
+    # unique, time, speed,
+    sample_entry = {
+        "uid" : uid,
+        "data" : [
+            # { "time" : time.time(), "speed" : 1.5 },
+            { "time" : 1566615167, "speed" : 1.5 },
+            { "time" : 1566615168, "speed" : 1.3 },
+            { "time" : 1566615169, "speed" : 1.1 },
+            { "time" : 1566615170, "speed" : 0.9 },
+        ]
+    }
+
+    # Insert entry
+    db.trackman.insert_one(sample_entry)
+
+    # Retrieve all DB entries
+    cursor = db.trackman.find()
+
+    # HTML start
+    html = """"
     <html>
         <head>
         </head>
-    <body>
-     <h1><u>RPi Tackman - Top Page</u></h1>
-     <br>
-     Hi, I'm GUID: {}<br>
-    </body>
-    </html>
+        <body>
+            <h1><u>RPi Tackman - Top Page</u></h1>
+            <br>
+            Hi, I'm GUID: {}<br>
     """.format(my_uuid)
+
+    for each_entry in cursor:
+        html = html + str(each_entry)
+
+    close_html = """
+        </body>
+    </html>
+    """
+
+    response = html + close_html
 
     return response
 
--- test_mongodb.py
+++ test_mongodb.py
@@ -1,30 +1,48 @@
 #! /usr/bin/python
+import os
 import time
+import json
 from pymongo import MongoClient
 import pymongo
 
-client = MongoClient('192.168.10.9:27017')
-db = client.rpi_trackman
-#print(db)
+# If running in PWS, use mlab
+if 'VCAP_SERVICES' in os.environ:
+	VCAP_SERVICES = json.loads(os.environ['VCAP_SERVICES'])
+	MONCRED = VCAP_SERVICES["mlab"][0]["credentials"]
+	client = MongoClient(MONCRED["uri"])
+	DB_NAME = str(MONCRED["uri"].split("/")[-1])
+	db = client[DB_NAME]
+
+else:
+	client = MongoClient('192.168.10.9:27017')
+	db = client.rpi_trackman
+	#print(db)
 
 #exit()
 
-t1 = int(time.time())
+uid = int(time.time())
 
-print(t1)
+print(uid)
 
 # Entry needed in DB
 # unique, time, speed,
 sample_entry = {
-	"uid" : time.time(),
+	"uid" : uid,
 	"data" : [
 		# { "time" : time.time(), "speed" : 1.5 },
 		{ "time" : 1566615167, "speed" : 1.5 },
 		{ "time" : 1566615168, "speed" : 1.3 },
 		{ "time" : 1566615169, "speed" : 1.1 },
+		{ "time" : 1566615170, "speed" : 0.9 },
 	]
 }
 
 print(sample_entry)
 
-# db.trackman.insert_one(sample_entry)
+# Insert entry
+db.trackman.insert_one(sample_entry)
+
+# Retrieve all
+cursor = db.trackman.find()
+for each_entry in cursor:
+	print(each_entry)
